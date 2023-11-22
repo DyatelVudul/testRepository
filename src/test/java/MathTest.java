9a4f1dda150d953507c1b0a7ca01c9705bdcff80
= <=>
2023-11-22 22:08:50 +0100

    initial push

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathTest {
+
+    @Test
+    public void TestAdd(){
+        assertEquals(2, Math.add(1,1));
+    }
+}
